1c86715a95e768730d2943fc968115a2929e5623
MikeHe2 <michael.he30@hotmail.com>
2024-10-02 00:16:36 -0400

    Added models for Users and Owner

--- probless-backend/account/models.py
+++ probless-backend/account/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
-# verifivar PermissionMixin
+# verify PermissionMixin for permissions
 
 class User(AbstractBaseUser):
 
