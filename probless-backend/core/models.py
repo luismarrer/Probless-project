b00e0f6e268a2a47ceceef1cbd20669ffdd3721e
Luismarrer <lmarrero1070@gmail.com>
2024-10-01 21:53:24 -0400

    Create Ticket, Workspace, Department models

--- probless-backend/core/models.py
+++ probless-backend/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Ticket(models.Model):
+	id = models.AutoField(primary_key=True)
+	title = models.CharField(max_length=255)
+	description = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
+	user_id = models.ForeignKey('user', on_delete=models.CASCADE)
+	priority = models.TextField()
+	assigned_department_id = models.ForeignKey('department', on_delete=models.CASCADE)
+	staus = models.TextField()
