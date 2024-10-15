e2a29e6f5a364463b60a3202c87cf287cfeb7d6b
nellyretto <danielys2416@gmail.com>
2024-10-15 13:01:09 -0400

    Update dist folder path in index.html and package.json

--- front-end/vue3-bootstrap5/vue.config.js
+++ front-end/vue3-bootstrap5/vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
+  publicPath: process.env.NODE_ENV === 'production' ? '/Probless-project/' : '/'
 });
